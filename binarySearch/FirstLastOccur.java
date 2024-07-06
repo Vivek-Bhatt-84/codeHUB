78413436b5f136297be52cd5486f1f5f0ab0e9bf
Vivek Bhatt <the9416legend@gmail.com>
2024-07-06 10:09:44 +0530

    2.

---
+++ binarySearch/FirstLastOccur.java
@@ -0,0 +1,49 @@
+package binarySearch;
+
+/*
+ * first and last occurance using binary search 
+ * using upper bound lower bound 
+ * brute force O(n).
+ */
+public class FirstLastOccur {
+    public static int first(int[] arr, int x) {
+        int n = arr.length;
+        int l = 0, h = n - 1, first = -1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (arr[mid] == x) {
+                first = mid;
+                h = mid - 1;
+            }
+            else if (arr[mid] < x) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+        return first;
+    }
+    public static int last(int[] arr, int x) {
+        int n = arr.length;
+        int l = 0, h = n - 1, last = -1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (arr[mid] == x) {
+                last = mid;
+                l = mid + 1;
+            }
+            else if (arr[mid] < x) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+        return last;
+    }
+    public static void main(String[] args) {
+        int[] arr = {  2 ,  8,  8 ,  8,  8,  8 , 11, 13 };
+        int x = 8;
+        int ans = last(arr, x);
+        System.out.println(ans);
+    }
+}
