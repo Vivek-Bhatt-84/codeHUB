451a04b335c26b904f13279db40b40ed3c686f9a
Vivek Bhatt <the9416legend@gmail.com>
2024-07-06 09:22:33 +0530

    lower , upper bound , floor and ceil

--- binarySearch/BinarySearch.java
+++ binarySearch/BinarySearch.java
@@ -1,8 +1,26 @@
 package binarySearch;
 
+/* 
+ * lower bound -> smallest index where arr[ind] >= x
+ * upper bound -> smallest index where arr[ind] > x 
+ */
+import java.util.Arrays;
 
+class Pair {
+    int floor, ceil;
+
+    Pair() {
+        this.floor = 0; // largest no in array <= target 
+        this.ceil = 0; // smallest no in array >= target 
+    }
+
+    Pair(int floor, int ceil) {
+        this.floor = floor;
+        this.ceil = ceil;
+    }
+}
 public class BinarySearch {
-    public static int bs(int[]arr,int target){
+    public static int bs(int[] arr, int target) {
         int low = 0;
         int high = arr.length - 1;
         while (low <= high) {
@@ -18,6 +36,45 @@ public class BinarySearch {
 
         }
         return 0;
+    }
+
+    public static Pair getFloorAndCeil(int[] arr, int n, int x) {
+        Arrays.sort(arr);
+        int getfloor = floor(arr, x);
+        int getCeil = ceil(arr, x);
+        return new Pair(getfloor, getCeil);
+
+    }
+
+    public static int floor(int[] arr ,int target) {
+         int s = 0 , e = arr.length - 1;
+         int ans = -1;
+         while (s <= e) {
+             int mid = s + (e - s) / 2;
+             if (arr[mid] <= target) {
+                 ans = mid;
+                 e = mid - 1;
+             }
+             else{
+                s=mid+1;
+             }
+        }
+        return ans;
+    }
+    public static int ceil(int[] arr ,int target) {
+         int s = 0 , e = arr.length - 1;
+         int ans = -1;
+         while (s <= e) {
+             int mid = s + (e - s) / 2;
+             if (arr[mid] >= target) {
+                 ans = mid;
+                 s = mid + 1;
+             }
+             else{
+                 e = mid - 1;
+             }
+        }
+        return ans;
     }
 public static void main(String[] args) {
     
